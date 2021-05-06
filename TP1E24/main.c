35048c290467fea6f12b5c110d7a05459491da5c
dam884 <dam884@users.noreply.github.com>
2021-05-06 11:17:58 -0300

    Se agrega TP1E24

---
+++ TP1E24/main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int main()
+{
+    // Desarrollar una función que elimine el elemento que ocupa una
+    // determinada posición en un arreglo de enteros.
+
+    int vec[]={23,54,67,45,0,0,0,0,0,0};
+    int* posLibre = &vec[5];
+
+    printf("La dirección de la primera posición libre es: %p", posLibre);
+
+
+
+    while(1)
+    {
+
+    }
+
+    return 0;
+}
